0cba4dff04ca030d8651496e54087412756dec5b
Neelansh Mathur <neelansh15@outlook.com>
2020-03-23 14:18:14 +0530

    Initial Commit

---
+++ app/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>Message from state using Vuex: {{ title }}</h2>
+    <form @submit.prevent="changeState()">
+      <input type="text" v-model="text" placeholder="Text">
+      <button type="submit">Change state!</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  computed: {
+    title(){
+      return this.$store.state.title
+    }
+  },
+  methods:{
+    changeState(){
+      this.$store.state.title = this.text
+    }
+  }
+}
+</script>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Current State: {{ title }}</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  },
+  computed:{
+    title(){
+      return this.$store.state.title
+    }
+  }
+}
+</script>
